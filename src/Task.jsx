a1a59ad61a2cd96c80274d515b0f942d8351dd8c
Juan Manuel Herrera Hernández <86576536+JuanM1228@users.noreply.github.com>
2023-01-19 23:23:28 -0600

    Stylesheets

---
+++ src/Task.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Stylesheets/Task.css";
+
+export function TaskCard() {
+  return (
+    <div className="card">
+      <h1> Titulo de la tarjeta</h1>
+      <p> Tarea realizada</p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -3,23 +3,13 @@ import ReactDOM from "react-dom";
 import { Greeting, UserCard } from "./Greeting";
 import Product from "./Product";
 import { Button } from "./Button"
+import { TaskCard } from "./Task";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 let alert = () => { alert("Hello") }
 root.render(
   <>
-    <Button text='Click me' />
-    <Button text='Pay' />
-    <Button />
-
-    {/*  <UserCard
-      name="Juan Herrera"
-      amount={300}
-      married={true}
-      points={[12, 234, 11, 10]}
-      address={{ street: "123 Main Street", city: "New York" }}
-      greet={alert}
-    /> */}
+    <TaskCard />
   </>
 );
