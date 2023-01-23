c2bd759e5f46f3cc0b212ff03e062d78588c4e04
Juan Manuel Herrera Hernández <86576536+JuanM1228@users.noreply.github.com>
2023-01-22 23:08:48 -0600

    Event Handlers

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,16 +1,22 @@
 import PropTypes from "prop-types";
 
 export function Button({ text }) {
-  console.log(text);
+  const GreetingButton = () => {
+    console.log("Hola mundo en consola");
+  };
+
   if (!text) {
     console.error("El texto es requerido");
   }
-  return <button>{text}</button>;
+  return <button onClick={GreetingButton}>{text}</button>;
 }
 
+/* Se coloca para requerir un argunmento */
 Button.propTypes = {
   text: PropTypes.string.isRequired,
 };
+
+/* Se coloca para colocar props default */
 
 Button.defaultProps = {
   text: "Default",
--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import { Greeting, UserCard } from "./Greeting";
 import Product from "./Product";
 import { Button } from "./Button"
 import { TaskCard } from "./Task";
-
+import { Saludar } from "./Saludar";
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
-let alert = () => { alert("Hello") }
+let evento = (e) => {
+  console.log(e.target.value + '...')
+}
 root.render(
   <>
     <TaskCard ready={true} />
+    <Saludar />
+    <Button text="Saludo" />
+    <input onChange={evento} />
   </>
 );
