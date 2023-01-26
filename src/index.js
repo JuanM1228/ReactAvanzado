8b965a4c720fa046073aa5315e6e01bf078d1129
Juan Manuel Herrera Hernández <86576536+JuanM1228@users.noreply.github.com>
2023-01-25 22:30:33 -0600

    Iteraciones

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ import Post from "./Post";
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
+const user = [{
+  id: 1,
+  name: 'Ryan Ray',
+  image: 'https://robohash.org/user 1'
+},
+{
+  id: 2,
+  name: 'Joe Wan',
+  image: 'https://robohash.org/user 1'
+}]
+
 root.render(
   <>
     <Post />
