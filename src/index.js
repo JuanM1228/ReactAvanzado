6f5737d2b6a60ed47986a9f1e0e454e45890fa43
Juan Manuel Herrera Hernández <86576536+JuanM1228@users.noreply.github.com>
2023-01-26 23:40:12 -0600

    ProtoArrays

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Post from "./Post";
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
-const user = [{
+const users = [{
   id: 1,
   name: 'Ryan Ray',
   image: 'https://robohash.org/user 1'
@@ -13,11 +13,13 @@ const user = [{
 {
   id: 2,
   name: 'Joe Wan',
-  image: 'https://robohash.org/user 1'
+  image: 'https://robohash.org/user 2'
 }]
 
 root.render(
   <>
-    <Post />
+    {users.map((user, index) => {
+      return <h1 key={index}>{user.name}</h1>
+    })}
   </>
 );
