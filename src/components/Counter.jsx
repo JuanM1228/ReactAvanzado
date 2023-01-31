61f3239575e197b974278a2df05a778200eb77f3
Juan Manuel Herrera Hernández <86576536+JuanM1228@users.noreply.github.com>
2023-01-30 23:50:46 -0600

    Clock Variables

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,19 +1,25 @@
 import { useState, useEffect } from "react";
 
 const Counter = () => {
-  const [clockState, setClockState] = useState("");
+  let now = new Date();
+  let actualHour = now.getHours();
+  const [clockState, setClockState] = useState("---");
+  const [seconds, setSeconds] = useState("");
+  const [minutes, setMinutes] = useState("");
+  const [hours, setHours] = useState(actualHour);
 
   useEffect(() => {
     setInterval(() => {
       const date = new Date();
       setClockState(date.toLocaleTimeString());
+      console.log(hours);
     }, 1000);
   });
   return (
     <>
       <h1 className="subTitle">useState & useEffect</h1>
       <div id="counter">
-        <p>{clockState}</p>
+        <p>{clockState + hours}</p>
         <div id="buttonsCounter">
           <button>Add</button>
           <button>Substract</button>
